a6f3357b503d391bd3c029592a2fe257b4f3edb0
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-05-31 11:12:11 -0300

    Cria Instalab

---
+++ modulo1/estados-insta4/insta4/src/components/Post/Post.js
@@ -0,0 +1,128 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+//o que o state está fazendo aqui? Quando ele é atualizado e quem chama seu valor atualizado?
+function Post(props){
+  const [state, setState] = useState({
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
+  })
+
+  const [numeroCurtidas, setnumeroCurtidas] = useState (0)
+  const [curtido, setCurtido] = useState(false)
+  const [comentando, setComentando] = useState(false)
+  const [numeroComentarios, setNumeroComentarios] = useState(0)
+  const [input, setInput] = useState('')
+  const [inputAtualizado, setInputAtualizado] = useState('')
+
+  const onClickCurtida = () => {
+    setCurtido(!curtido)
+    if(!curtido){
+    setnumeroCurtidas(numeroCurtidas + 1)}
+    else{
+      setnumeroCurtidas(numeroCurtidas - 1)
+    }
+  }
+  const handleInput = (event) => {
+    setInput(event.target.value)
+  }
+  const onClickComentario = () => {
+    setComentando(!comentando)
+    if(comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    }
+    
+  }
+  
+  const aoEnviarComentario = () => {
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
+    setInputAtualizado(input)
+    setInput('')
+    
+  }
+  console.log(input)
+  
+  let iconeCurtida
+
+    if(curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(comentando) {
+      componenteComentario = <SecaoComentario value = {input} onChangeComentario= {handleInput} aoEnviar={aoEnviarComentario}/>
+    }
+
+  return(
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  )
+}
+
+
+export default Post
---
+++ modulo1/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+export function SecaoComentario(props) {
+	return (
+		<CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				value={props.value}
+				onChange={props.onChangeComentario}
+			/>
+			<button onClick={props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+	)
+}
+
+//pegar o valor do comentário e atualizar o estado --feito --falta criar componente que recebe o valor e imprime na tela
+//falta definir regra para que só conte como comentário se input.length > 0
+//
