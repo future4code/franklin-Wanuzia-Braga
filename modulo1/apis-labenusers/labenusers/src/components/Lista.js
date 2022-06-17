98bfd91da100969fbe3eb941b9f8bbdb0af89b32
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-06-16 22:20:30 -0300

    cria botão para buscar usuário por nome

--- modulo1/apis-labenusers/labenusers/src/components/Lista.js
+++ modulo1/apis-labenusers/labenusers/src/components/Lista.js
@@ -3,7 +3,11 @@ import axios from "axios";
 
 const Lista = () => {
    const [listaUsuarios, setListaUsuarios] = useState([])
-   
+   const [inputNomeUsuario, setInputNomeUsuario] = useState('')
+
+   const handleInputUser = (event) =>{
+    setInputNomeUsuario(event.target.value)
+}
    const exibeListaDeUsuarios = () => {
     axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
       headers: {
@@ -14,20 +18,41 @@ const Lista = () => {
       console.log(response)
     }).catch((error) => {
       console.log(error.code)
-    })}
-
+    })};
+      
+    const exibeUsuarioPorNome = () =>{
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/search?name',
+        {
+            params:{
+                name: inputNomeUsuario
+            }
+        }, 
+        {
+            headers: {
+                Authorization: 'wanuzia-braga-franklin'
+            }
+        },
+        ).then((response) => {
+            setListaUsuarios(response.data)
+            setInputNomeUsuario('')
+            exibeListaDeUsuarios();
+            console.log(inputNomeUsuario)
+        }).catch(() => {
+            alert(`Usuário ${inputNomeUsuario} não encontrado`)
+        })
+    }   
     const ListaDeUsuarios =  listaUsuarios.length > 0 ?  listaUsuarios.map((usuario) => {
         return <p key={usuario.id}>{usuario.name}</p>
        }) : "Sem usuários cadastrados"
-       
+   
      useEffect(exibeListaDeUsuarios, [])
 
    return(
       <div>
          <h1>Usuários</h1>
-        {ListaDeUsuarios}
-         {/* <input value={inputUser} onChange={handleInputUser}/><button onClick={exibeListaDeUsuarios}>Procurar Usuário</button> */}
-      </div>
+         <input value={inputNomeUsuario} onChange={handleInputUser} placeholder='Insira o nome do usuário'/><button onClick={exibeUsuarioPorNome}>Procurar Usuário</button>
+         {ListaDeUsuarios}
+     </div>
    )
 }
 
