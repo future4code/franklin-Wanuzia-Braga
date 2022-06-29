9d2961707285d578ae5d913540787acc1a0b18e2
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-06-28 21:43:19 -0300

    Adiciona formulário

---
+++ modulo1/projeto-cookenu/cookenu/src/pages/LoginPage/LoginForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { InputsContainer } from "./styled";
+import TextField from '@material-ui/core/TextField'
+import useForm from '../../hooks/useForm'
+import { Button } from "@material-ui/core";
+import axios from 'axios';
+import { BASE_URL } from "../../constants/urls";
+
+const LoginForm = () => {
+    const [form, onChange, clear] = useForm({email:'', password: ''})
+
+    const onSubmitForm = (e) => {
+        console.log(form)
+        e.preventDefault()
+        login()
+    }
+    const login = () => {
+        axios.post(`${BASE_URL}/user/login`, form)
+        .then((res) => {
+            console.log(res)})
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+    return(
+           <InputsContainer>
+            <form onSubmit={onSubmitForm}>
+             <TextField 
+             name={'email'} 
+             value={form.email}
+             onChange={onChange}
+             label={'E-mail'}
+             variant={"outlined"}
+             fullWidth
+             margin={'normal'}
+             required
+             type={'email'}
+             />
+            <TextField 
+             name={'password'} 
+             value={form.password}
+             onChange={onChange}
+             label={'Senha'}
+             variant={"outlined"}
+             fullWidth
+             margin={'normal'}
+             required
+             type={'password'}
+             />
+             <Button
+             type={'submit'}
+             fullWidth
+             variant={'contained'}
+             color={'primary'}
+             margin={'normal'}
+             >Fazer login</Button>
+            </form>
+            </InputsContainer>
+             )
+}
+
+export default LoginForm
--- modulo1/projeto-cookenu/cookenu/src/pages/LoginPage/LoginPage.js
+++ modulo1/projeto-cookenu/cookenu/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { LogoImage, ScreenContainer, SignUpButtonContainer } from "./styled";
+import logo from '../../assets/logo.png'
+import { Button } from "@material-ui/core";
+import LoginForm from "./LoginForm";
+import {useNavigate} from 'react-router-dom';
+import { goToSignUp } from "../../routes/coordinator";
 
 const LoginPage = () => {
-
-    return(
-        <div>
-            <h1>LoginPage</h1>
-        </div>
+    const navigate = useNavigate()
+       return(
+        <ScreenContainer>
+            < LogoImage src={logo} />
+           <LoginForm />
+            <SignUpButtonContainer>
+                <Button 
+                onClick={() => goToSignUp(navigate)}
+                type={'submit'}
+                fullWidth
+                variant={'text'}
+                 color={'primary'}
+                 margin={'normal'}
+                >Não possui conta? Cadastre-se</Button>
+            </SignUpButtonContainer>
+        </ScreenContainer>
     )
 }
 
