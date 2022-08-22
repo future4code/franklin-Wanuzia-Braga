5747639ac74046a5d91be24dfd4f58cd83099dd2
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-22 12:19:37 -0300

    Cria primeiros endpoints user

--- modulo3/toDoList/src/app.ts
+++ modulo3/toDoList/src/app.ts
@@ -1,31 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
-import connection from "./connection";
-import { Response, Request } from "express";
+
 
 
 const app = express();
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3008
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/testeList", async (req:Request, res:Response) => {
-  const result = await connection.select()
-  .from('TodoListUser')
-  .innerJoin('id', 'name', 'nickname', 'email')
-  .then((result) => {
-    return res.status(200).json({result})
-  })
-  .catch((err) => {
-    return res.status(400)
-  })
-  return res.status(200).send({message:`${result}`})
-  
-})
-
-
 
 
 const server = app.listen(port, () => {
