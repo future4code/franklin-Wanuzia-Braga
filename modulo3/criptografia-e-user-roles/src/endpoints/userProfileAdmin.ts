601cf784c9b51da36175c8440b694a683f7d0d04
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-12 13:38:56 -0300

    Adiciona endpoint que retorna info de usuário

--- modulo3/criptografia-e-user-roles/src/endpoints/userProfileAdmin.ts
+++ modulo3/criptografia-e-user-roles/src/endpoints/userProfileAdmin.ts
@@ -3,17 +3,17 @@ import { UserDatabase } from "../database/UserDatabase";
 import Authenticator from "../services/Authenticator";
 import { authenticationData, UserRole } from "../types";
 
-export const getById = async (req:Request, res:Response) => {
+export const acessoAdmin = async (req:Request, res:Response) => {
     const token = req.headers.authorization as string;
     try{
         const data = new Authenticator().compare(token);
         const user = await new UserDatabase().getUserById(data.id);
-
-        res.status(200).send({
-            id:user.id, 
-            email:user.email, 
-            role:user.role
-        });
+    //verifica se o usuário pode acessar esse endpoint:
+      if(data.role !== UserRole.ADMIN){
+        res.statusCode = 403;
+        res.send("Usuário não autorizado.")
+     }
+        res.status(200).send({id:user.id, email:user.email, role:user.role});
     }catch(error){
         res.send(error)
     }
--- modulo3/criptografia-e-user-roles/src/index.ts
+++ modulo3/criptografia-e-user-roles/src/index.ts
@@ -1,11 +1,13 @@
 import app from "./app"
 import createUser from "./endpoints/createUser";
 import { deleteUser } from "./endpoints/deleteUser";
-import { acessoAdmin } from "./endpoints/getUserById";
+import { acessoAdmin } from "./endpoints/userProfileAdmin";
 import { login } from "./endpoints/login";
+import { getById } from "./endpoints/getUserById";
 
 
 app.post("/user", createUser);
 app.post("/user/login", login);
 app.get("/user/profile", acessoAdmin);
-app.delete("/user/:id", deleteUser)
+app.delete("/user/:id", deleteUser);
+app.get("/user", getById)
