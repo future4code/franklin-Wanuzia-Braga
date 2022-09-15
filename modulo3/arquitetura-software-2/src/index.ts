033a781aed033b2d48049b73c2cb3969c971105b
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-15 17:39:31 -0300

    Adiciona o router

--- modulo3/arquitetura-software-2/src/index.ts
+++ modulo3/arquitetura-software-2/src/index.ts
@@ -1,12 +1,5 @@
 import app from "./app"
-import { UserController } from "./controller/UserController"
+import { userRouter } from "./router/userRouter"
 
 
-
-const userController = new UserController()
-
-app.post("/users/signup", userController.signup)
-app.post("/users/login", userController.login)
-app.get("/users/", userController.getUsers)
-app.delete("/users/:id", userController.deleteUser)
-app.put("/users/:id", userController.editUser)
+app.use("/users", userRouter)
