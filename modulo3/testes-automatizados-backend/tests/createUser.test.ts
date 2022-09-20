eecbde324e4e92ffc5ac6c24736d0fe741adf26f
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-20 12:14:33 -0300

    teste que força um erro no banco de dados

--- modulo3/testes-automatizados-backend/tests/createUser.test.ts
+++ modulo3/testes-automatizados-backend/tests/createUser.test.ts
@@ -2,16 +2,16 @@ import { BaseDatabase } from "../src/cookenu-backend/BaseDatabase"
 import { User, USER_ROLES } from "../src/cookenu-backend/models/User";
 import { UserDatabase } from "../src/cookenu-backend/UserDatabase";
 
-describe("Testing Database", () => {
+describe("Testing async functions", () => {
 test("should connect to MySql", async () => {
     const [tables] = await BaseDatabase.connection.raw(
         "SHOW TABLES;"
     )
-    console.log(tables);
-    expect(tables).toBeDefined()
 
-    })
-});
+    expect(tables).toBeDefined();
+    expect(tables).toContainEqual({
+        'Tables_in_franklin-wanuzia-braga': 'Cookenu_Users'
+ })});
 
 test("Should create an user", async () => {
     const user: User = new User (
@@ -40,5 +40,25 @@ test("Should create an user", async () => {
   afterAll(async () => {
     await new UserDatabase().deleteUser("abc-123-456")
     await BaseDatabase.connection.destroy()
-})
+  });
 
+  test("should return  1 asssertion", async () => {
+      expect.assertions(1)
+    try {
+        const user: User = new User (
+            "abc-123-456",
+            "Wanuzia",
+            "wanuzia@email.com",
+            "abc123",
+            USER_ROLES.ADMIN
+    ) 
+  
+    await new UserDatabase().createUser(user)
+    await new UserDatabase().createUser(user)
+
+    } catch (err) {
+      expect(err).not.toBe(undefined)
+    }
+  });
+
+});
