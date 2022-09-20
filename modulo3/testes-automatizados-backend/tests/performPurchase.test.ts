b18b1b446598e392334b16cc928abd34d74f5657
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-19 21:41:16 -0300

    Realiza exercício 1

---
+++ modulo3/testes-automatizados-backend/tests/performPurchase.test.ts
@@ -0,0 +1,39 @@
+import { performPurchase, User } from "../src";
+
+describe("Testing performPurchase function", () => {
+   test("Testing balance greater than value", () => {
+    const user: User = {
+        name: "Wanuzia", 
+        balance: 500
+    }
+    const result = performPurchase(user, 100)
+
+    expect(result).toEqual({
+        name: "Wanuzia",
+        balance: 400
+    })
+});
+test("Testing balance equal than value", () => {
+    const user: User = {
+        name: "Wanuzia", 
+        balance: 200
+    }
+    const result = performPurchase(user, 200)
+
+    expect(result).toEqual({
+        name: "Wanuzia",
+        balance: 0
+    })
+});
+test("Testing balance less than value", () => {
+    const user: User = {
+        name: "Wanuzia", 
+        balance: 200
+    }
+    const result = performPurchase(user, 400)
+
+    expect(result).not.toBeDefined()
+});
+})
+
+
